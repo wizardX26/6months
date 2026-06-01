5a8f3507935b3c378d7f90313c92254e04741dac
the_wizard <wizard.os25@proton.me>
2026-06-01 21:36:32 +0700

    Add ChipPage project

---
+++ ChipPage/ChipPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChipPage
+//
+//  Created by wizard.os25 on 1/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
